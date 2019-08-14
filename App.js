36994341a6e35ff338b32fbf417298f8b466d194
MrsGuo <972134688@qq.com>
2019-08-14 15:42:56 +0800

    初始化项目

---
+++ App.js
@@ -0,0 +1,39 @@
+import {
+  Home,
+  Business,
+  More,
+  Mine
+} from './Views';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          App
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+});
---
+++ Views/index.js
@@ -0,0 +1,13 @@
+import Business from './Business';
+import Home from './Home';
+import Mine from './Mine';
+import More from './More';
+
+
+
+export {
+  Business,
+  Home,
+  Mine,
+  More
+};
